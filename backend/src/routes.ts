24c1949911b530211a53c365d30fddf0bf12463c
jbresolinn <juliabresolin2@gmail.com>
2020-08-01 17:37:15 -0300

    feat: register point collect without image upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,7 +7,8 @@ routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*');
 
   const serializedItems = items.map(item => {
-    return { 
+    return {
+      id: item.id, 
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     }
@@ -16,5 +17,35 @@ routes.get('/items', async (request, response) => {
 
   return response.json(serializedItems);
 });
+
+routes.post('/points', async (request, response) => {
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({ 
+    name, 
+    email, 
+    whatsapp, 
+    latitude, 
+    longitude, 
+    city, 
+    uf,
+    image: 'image-fake' 
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map( (item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    }
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ sucess: true });
+})
 
 export default routes;
